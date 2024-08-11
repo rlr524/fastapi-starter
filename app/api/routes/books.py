92c99259e94207ebe69fc65bf1e90c965db4eea1
Rob Ranf <rob@emiyaconsulting.com>
2024-08-11 14:46:55 -0700

    added get favorite book route and handler

--- app/api/routes/books.py
+++ app/api/routes/books.py
@@ -4,12 +4,18 @@ router = APIRouter()
 
 
 BOOKS = [
-    {"id": "1", "title": "Title Three", "author": "Author Three", "category": "science"},
-    {"id": "2", "title": "Title Two", "author": "Author Two", "category": "history"},
-    {"id": "3", "title": "Title Four", "author": "Author Four", "category": "science"},
-    {"id": "4", "title": "Title Five", "author": "Author Five", "category": "math"},
-    {"id": "5", "title": "Title Six", "author": "Author Six", "category": "languages"},
-    {"id": "6", "title": "Title One", "author": "Author One", "category": "science"},
+    {"id": "1", "title": "Title Three", "author": "Author Three", "category":
+        "science", "favorite": False},
+    {"id": "2", "title": "Title Two", "author": "Author Two", "category": "history",
+     "favorite": False},
+    {"id": "3", "title": "Title Four", "author": "Author Four", "category": "science",
+     "favorite": True},
+    {"id": "4", "title": "Title Five", "author": "Author Five", "category": "math",
+     "favorite": False},
+    {"id": "5", "title": "Title Six", "author": "Author Six", "category": "languages",
+     "favorite": False},
+    {"id": "6", "title": "Title One", "author": "Author One", "category": "science",
+     "favorite": True},
 ]
 
 
@@ -18,10 +24,23 @@ async def get_books():
     return BOOKS
 
 
-@router.get("/books/{p}")
-async def get_book(p):
+@router.get("/books/mybook")
+async def get_favorite_books():
+    user_favorites = []
     for book in BOOKS:
-        if book["id"] is p:
+        if book.get("favorite") is True:
+            user_favorites.append(book)
+
+    if len(user_favorites) == 0:
+        return "no books marked as a favorite, yet"
+    else:
+        return user_favorites
+
+
+@router.get("/books/{p}")
+async def get_book(p: str):
+    for book in BOOKS:
+        if book.get("id") == p:
             return book
     else:
-        return "No book found with that ID"
+        return "no books with that id"
